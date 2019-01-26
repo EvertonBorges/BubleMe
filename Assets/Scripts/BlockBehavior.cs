86587c3ca692c881ccc3f8f399ad19f825835f57
EVERTON-NOTEBOO\Everton <2014118970020@gmail.com>
2019-01-26 09:44:05 -0400

    Commit inicial.

---
+++ Assets/Scripts/BlockBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start() {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.position = offset + target.position;
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBehavior : MonoBehaviour {
+
+    private new Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private float speedForce;
+    private float realSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private bool isJumping;
+    private bool isGround;
+
+    // Start is called before the first frame update
+    void Start() {
+        realSpeed = speedForce;
+    }
+
+    private void Awake() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        isJumping = false;
+        isGround = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horinzontalAxis = Input.GetAxis("Horizontal");
+        float jumpAxis = Input.GetAxis("Jump");
+
+        if (horinzontalAxis != 0f) {
+            rigidbody2D.AddForce(Vector2.right * realSpeed * horinzontalAxis);
+        }
+
+        if (jumpAxis > 0f && !isJumping) {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+            isJumping = true;
+            Debug.Log("Pulou");
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (!isGround) {
+            if (collision.collider.CompareTag("Ground")) {
+                realSpeed = speedForce;
+                isJumping = false;
+                isGround = true;
+                Debug.Log("Está no chão");
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (isGround) {
+            if (collision.collider.CompareTag("Ground")) {
+                realSpeed = speedForce / 2;
+                isJumping = true;
+                isGround = false;
+                Debug.Log("Fora do chão");
+            }
+        }
+    }
+}
