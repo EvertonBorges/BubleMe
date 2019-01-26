d9dada47bf47d888af3c6ceb78408a7a76674816
EVERTON-NOTEBOO\Everton <2014118970020@gmail.com>
2019-01-26 11:04:06 -0400

    Adicionada rampa; Adicionado item de dar vida; Adicionando UI para a vida; Adicionada parede esquerda limitando o movimento.

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    private float offsetX;
+    private float startY;
+
+    void Awake() {
+        offsetX = transform.position.x - target.position.x;
+        startY = transform.position.y;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(offsetX + target.position.x, startY, -10f);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class PlayerController : MonoBehaviour {
 
+    private int life;
 
+    void Awake() {
+        life = 1;
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -15,4 +20,24 @@ public class PlayerController : MonoBehaviour {
     void Update() {
         
     }
+
+    public void addLife() {
+        addLife(1);
+    }
+
+    public void removeLife() {
+        addLife(-1);
+    }
+
+    public void addLife(int lifeToAdd) {
+        life += lifeToAdd;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Life")) {
+            GameObject.Destroy(collision.gameObject);
+            addLife();
+        }
+    }
+
 }
