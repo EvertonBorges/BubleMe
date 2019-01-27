fe91bf48f29951eb8e15ded0c9d46efea6f08abf
EVERTON-NOTEBOO\Everton <2014118970020@gmail.com>
2019-01-26 23:05:20 -0400

    Novos obstáculos e animação da mão.

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -7,14 +7,23 @@ public class CameraBehavior : MonoBehaviour {
     [SerializeField]
     private Transform target;
 
+    private float startSize;
     private float offsetX;
-    private float startX;
+    private float minX;
+    private float maxX;
     private float startY;
 
+    private Vector3 largeScale;
+    private float largeSize;
+
     void Awake() {
+        startSize = Camera.main.orthographicSize;
         offsetX = transform.position.x - target.position.x;
-        startX = transform.position.x;
+        minX = 01.7f;
+        maxX = 87.4f;
         startY = transform.position.y;
+
+        largeScale = new Vector3(43f, 21.4f, -10f);
     }
 
     // Start is called before the first frame update
@@ -24,6 +33,19 @@ public class CameraBehavior : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.position = new Vector3(((offsetX + target.position.x) < startX) ? startX : (offsetX + target.position.x), startY, -10f);
+        transform.position = new Vector3(PosicaoCameraX(), startY, -10f);
     }
+
+    private float PosicaoCameraX() {
+        float suggestedX = offsetX + target.position.x;
+
+        if (suggestedX < minX) {
+            return minX;
+        } else if (suggestedX > maxX) {
+            return maxX;
+        } else {
+            return suggestedX;
+        }
+    }
+
 }
